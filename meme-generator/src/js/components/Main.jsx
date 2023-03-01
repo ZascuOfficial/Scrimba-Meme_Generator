fb09ba52347ce3755c9ccb2f0aeab5973868cc56
Zascu <zascu.github@gmail.com>
2023-03-02 01:14:35 +0200

    Begin rendering the meme component

--- meme-generator/src/js/components/Main.jsx
+++ meme-generator/src/js/components/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Form from './Form';
+import Meme from './Meme';
 
 export default function Main() {
   return (
     <main>
       <Form />
+      <Meme />
     </main>
   );
 }
